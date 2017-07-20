c7af3c7a64fd788aa78283cd4dcb642d181b5366
liujun5 <linbsd@gmail.com>
2017-07-20 13:49:49 +0800

    test weibo

---
+++ weibo/weibo_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/larrylv/go-weibo/weibo"
+)
+func main() {
+	fmt.Print("Hello,World\n")
+	var accessToken = ""
+
+	accessToken = "access_token"
+	client := weibo.NewClient(accessToken)
+	print(client.BaseURL.Host)
+
+	// list  weibo
+	opts := &weibo.StatusListOptions{}
+	status, _, err := client.Statuses.UserTimeline(opts)
+	fmt.Printf("Status is :\n",status)
+	fmt.Printf("Err is :\n",err)
+
+}
+
